4af425c257f12ca8df46a42763d5d52d03e0262e
hasnain ahmad <hasnainahmad4890@gmail.com>
2023-01-01 23:30:39 +0500

    Designed skeleton of website , done some styling , make normal flow of project and APis calls normally we do ,then created Redux store , cartSlice for showing data in carts , made two functions of ADD() and Remove()

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -15,7 +15,7 @@ const cartSlice = createSlice({
       state.push(action.payload);
     },
     remove(state, action) {
-      state.filter((item) => item.id !== action.payload);
+      return state.filter((item) => item.id !== action.payload);
     },
   },
 });
---
+++ src/store/productSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const STATUSES = object.freeze({
+  IDLE: "idle",
+  ERROR: "error",
+  LOADING: "loading",
+});
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+  // Reducers are functions jis ki wajah sy hum state mutate karty hain
+  // Reducers are pure functions with no side effects , function ky bahir ka data change nhe karti
+
+  reducers: {
+    setProducts(state, action) {
+      //Dont do asynchronous calls here :NEVER
+      // bcz jo reducers hoty hain , wo SYNCHRONOUSLY call hoty hain( thet have side effects bcz error and delays may be possible)
+      //const res = await fetch("https://dummyjson.com/products");
+
+      // so far requests , we can use MIddleWares
+
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { setProducts, setStatus } = productSlice.actions;
+export default productSlice.reducer;
+
+// THUNKS
+//        its a middle ware in redux  , its a programming term that means
+// A piece of code that does some delayed work
+// Note : its a function itself , but function k andar function return karta hai
+export function fetchProducts() {
+  return async function fetchProdyctThunk(dispatch, getState) {
+    dispatch(setStatus(STATUSES.LOADING));
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      dispatch(setProducts(data));
+    } catch (err) {}
+  };
+}
